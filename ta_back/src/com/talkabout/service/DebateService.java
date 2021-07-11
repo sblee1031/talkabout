ffd61e60cf8ae7e66aa3c335d59b3792449b74d7
leebbong <56666728+sblee1031@users.noreply.github.com>
2021-07-11 17:12:48 +0900

    토론모집 페이징 처리 완료

--- ta_back/src/com/talkabout/service/DebateService.java
+++ ta_back/src/com/talkabout/service/DebateService.java
@@ -7,6 +7,8 @@ import java.util.Properties;
 import com.talkabout.dao.DebateDAO;
 import com.talkabout.dto.Debate;
 import com.talkabout.dto.DebateDetail;
+import com.talkabout.dto.Member;
+import com.talkabout.exception.ModifyException;
 
 public class DebateService {
 	private DebateDAO dao;
@@ -39,5 +41,33 @@ public class DebateService {
 	
 	public void addDebate(Debate deb, DebateDetail dd, String discuss1, String discuss2) {
 		dao.insertDebate(deb, dd,discuss1,discuss2);
+	}
+	public void addDiscussor(Debate deb_no, DebateDetail dd, Member m) {
+		dao.updateDiscussor(deb_no, dd, m);
+	}
+	public void cancleDiscussor(Debate deb_no, DebateDetail dd, Member m) {
+		dao.cancleDiscussor(deb_no, dd, m);
+	}
+	public void updateDebateAll(Debate deb, List<DebateDetail> dd, String discuss1,String discuss2) throws ModifyException{
+		dao.updateDebateAll(deb, dd,discuss1,discuss2);
+	}
+	
+	public void updateStatus(Debate status) {
+		dao.updateStatus(status);
+	}
+	public void deleteDebate(Debate deb_no) {
+		dao.deleteDebate(deb_no);
+	}
+	public List<Debate> selectSearch(String column, String keyword){
+		return dao.selectSearch(column, keyword);
+	}
+	public int lastRow() {//총 게시물 개수 구하기
+		return dao.lastRow();
+	}
+	public void pageNum(int page) {
+		dao.pageNum(page);
+	}
+	public void pageSize(int size) {
+		dao.pageSize(size);
 	}
 }
