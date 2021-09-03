d3909bd3966686ac88090f07fc1ecade884bfa35
leebbong <56666728+sblee1031@users.noreply.github.com>
2021-09-03 09:50:54 +0900

    페이징 버그 수정 (다음, 이전 버튼 위치 이동 오류)

--- notice/src/component/notice/NoticePaging.jsx
+++ notice/src/component/notice/NoticePaging.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Pagination = (props) => {
   //const pageCount = 5; //한페이지 표시될 개수
   const { itemsCount, pageSize, currentPage, onPageChange } = props; // 각각 아이템(영화목록) 개수, 한 페이지에 보여줄 아이템(영화목록) 개수
-  const totalPage = Math.ceil(itemsCount / pageSize); // 몇 페이지가 필요한지 계산 토탈페이지수
+  var totalPage = Math.ceil(itemsCount / pageSize); // 몇 페이지가 필요한지 계산 토탈페이지수
   const pageGroup = Math.ceil(currentPage / pageSize); //한 페이지에 보여줄 페이지 그룹수
   if (totalPage === 1) return null; // 1페이지 뿐이라면 페이지 수를 보여주지 않음
 
@@ -15,12 +15,18 @@ const Pagination = (props) => {
   if (first < 1) first = 1;
   var next = last + 1;
   var prev = first - 1;
-  // console.log("last", last);
-  // console.log("next", next);
-  // console.log("prev", prev);
-  // console.log("totalPage", totalPage);
-  // console.log("pageGroup", pageGroup);
-  // console.log("pageGroup", pageGroup);
+  var prevButton = false;
+  if (currentPage == 1) {
+    prevButton = false;
+  } else {
+    prevButton = true;
+  }
+  var nextButton = false;
+  if (currentPage == totalPage) {
+    nextButton = false;
+  } else {
+    nextButton = true;
+  }
   const pages = _.range(first, last + 1); // 마지막 페이지에 보여줄 컨텐츠를 위해 +1, https://lodash.com/docs/#range 참고
 
   return (
@@ -28,11 +34,16 @@ const Pagination = (props) => {
       {" "}
       {/* VSCode 입력: nav>ul.pagination>li.page-item>a.page-link */}
       <ul className="pagination">
-        <li className="prev">
-          <Link className="page-link" onClick={() => onPageChange(prev)}>
-            &laquo;
-          </Link>
-        </li>
+        {prevButton && (
+          <li className="prev">
+            <Link
+              className="page-link"
+              onClick={() => onPageChange(currentPage - 1)}
+            >
+              &laquo;
+            </Link>
+          </li>
+        )}
         {pages.map((page) => (
           <li
             key={page}
@@ -45,11 +56,16 @@ const Pagination = (props) => {
             {/* 페이지 번호 클릭 이벤트 처리기 지정 */}
           </li>
         ))}
-        <li className="next">
-          <Link className="page-link" onClick={() => onPageChange(next)}>
-            &raquo;
-          </Link>
-        </li>
+        {nextButton && (
+          <li className="next">
+            <Link
+              className="page-link"
+              onClick={() => onPageChange(currentPage + 1)}
+            >
+              &raquo;
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
