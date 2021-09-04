66cc5f13a76b20953b4623238bf35d7ac079a991
leebbong <56666728+sblee1031@users.noreply.github.com>
2021-09-04 22:01:32 +0900

    WebSocket 추가

---
+++ talkaboutspringboot/src/main/java/com/talkabout/configuration/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.talkabout.configuration;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Controller
+@Configurable
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000","http://localhost:8888","http://192.168.0.33:3000","http://172.30.1.14:3000").withSockJS();
+	}
+}
+
+//http://localhost:3000","http://localhost:8888","http://192.168.0.33:3000","http://172.30.1.14:3000
